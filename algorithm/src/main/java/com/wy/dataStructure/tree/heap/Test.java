02e6917715363ad7cdc95ae611443a8ce5d70261
卫印 <yin.wei@lanmaoly.com>
2019-12-23 11:34:57 +0800

    提交测试2

--- algorithm/src/main/java/com/wy/dataStructure/tree/heap/Test.java
+++ algorithm/src/main/java/com/wy/dataStructure/tree/heap/Test.java
@@ -11,6 +11,6 @@ public class Test {
         int[] eles = new int[]{0, 14, 7, 30, 2, 15, 17};
         Heap.sorted(eles);
         IntStream.of(eles).forEach(System.out::println);
-        System.out.println("提交测试");
+        System.out.println("提交测试2");
     }
 }
