8a1591f9080ba4b3197eec5b59758509bb49cfe7
卫印 <yin.wei@lanmaoly.com>
2020-03-11 21:07:10 +0800

    finally

---
+++ jvm/src/main/java/com/wy/base/Finally.java
@@ -0,0 +1,26 @@
+package com.wy.base;
+
+/**
+ * 1、finally并不会影响try catch中的return结果，因为 return结果在执行finally时已经额外保存起来了，
+ *         在finally执行完毕后，将之前保存的结果压入当前桢栈。
+ * 2、但是如果finally中执行return语句的话，会提前终止方法调用，直接返回finally的return结果
+ *
+ * @author Dwen
+ * @date 2020-03-11 17:11
+ */
+public class Finally {
+    public static void main(String[] args) {
+        Finally f = new Finally();
+        System.out.println(f.f());
+    }
+
+    public int f(){
+        int a = 0;
+
+        try {
+            return a;
+        } finally {
+             a++;
+        }
+    }
+}
